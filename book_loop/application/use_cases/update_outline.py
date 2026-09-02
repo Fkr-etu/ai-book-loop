b4a6ff72b4a83345ef45fa8819a9a0591002b86c
Florian Kromer <florian.kromer@telecomnancy.net>
2026-09-02 08:24:41 +0200

    feat: validate editable structured outlines

--- book_loop/application/use_cases/update_outline.py
+++ book_loop/application/use_cases/update_outline.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from book_loop.domain.models import BookState
+from book_loop.domain.models import BookState, Outline
 from book_loop.domain.protocols import BookRepository
 
 
@@ -10,10 +10,9 @@ class UpdateOutline:
     def __init__(self, repository: BookRepository) -> None:
         self.repository = repository
 
-    def execute(self, book: BookState, *, outline: str) -> BookState:
-        if not outline.strip():
+    def execute(self, book: BookState, *, outline: Outline) -> BookState:
+        if not outline.chapters:
             raise ValueError("The outline cannot be empty")
-
         book.outline = outline
         book.outline_approved = False
         self.repository.save(book)
