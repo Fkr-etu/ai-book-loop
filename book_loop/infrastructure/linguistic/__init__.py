6eedcf034de791c52c8d3727e2e724998f4909c6
Florian Kromer <florian.kromer@telecomnancy.net>
2026-09-04 15:51:40 +0200

    feat: expose linguistic checker adapters

---
+++ book_loop/infrastructure/linguistic/__init__.py
@@ -0,0 +1,4 @@
+from book_loop.infrastructure.linguistic.languagetool import LanguageToolChecker
+from book_loop.infrastructure.linguistic.spacy import SpacyFrenchChecker
+
+__all__ = ["LanguageToolChecker", "SpacyFrenchChecker"]
