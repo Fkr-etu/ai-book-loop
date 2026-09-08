a4601e8b86fef956a69b8fb00d8742f64a0dcd73
Florian Kromer <florian.kromer@telecomnancy.net>
2026-09-08 13:30:36 +0200

    style: order outline dependency imports

--- book_loop/api/routes/outline.py
+++ book_loop/api/routes/outline.py
@@ -5,7 +5,7 @@ from typing import Any
 from fastapi import APIRouter, Depends, HTTPException, Request
 from pydantic import BaseModel
 
-from book_loop.api.dependencies import get_owned_book, get_container
+from book_loop.api.dependencies import get_container, get_owned_book
 from book_loop.domain.models import Outline
 from book_loop.infrastructure.container import Container
 
